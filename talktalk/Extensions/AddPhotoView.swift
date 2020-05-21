7e80c9572e17ebb7b79175256cd8f05b7ff7b6ba
Boris Goncharov <bora4bora@gmail.com>
2020-05-20 19:56:31 -0700

    add photo view

--- talktalk/Extensions/AddPhotoView.swift
+++ talktalk/Extensions/AddPhotoView.swift
@@ -39,9 +39,32 @@ class AddPhotoView: UIView {
     
     private func setupConstraints() {
         
+        NSLayoutConstraint.activate([
+            circleImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            circleImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            circleImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+            circleImageView.heightAnchor.constraint(equalToConstant: 100),
+            circleImageView.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        NSLayoutConstraint.activate([
+            plusButton.trailingAnchor.constraint(equalTo: circleImageView.trailingAnchor, constant: 0),
+            plusButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            plusButton.heightAnchor.constraint(equalToConstant: 30),
+            plusButton.widthAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        self.bottomAnchor.constraint(equalTo: circleImageView.bottomAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: plusButton.trailingAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        circleImageView.layer.masksToBounds = true
+        circleImageView.layer.cornerRadius = circleImageView.frame.width / 2
     }
 }
