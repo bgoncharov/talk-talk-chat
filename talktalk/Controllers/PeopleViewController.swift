b8b9e8ec9c70bc2f4ae1b09f20223d69f6a8b8c8
Boris Goncharov <bora4bora@gmail.com>
2020-05-21 12:08:23 -0700

    composition layot

---
+++ talktalk/Controllers/PeopleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleViewController.swift
+//  talktalk
+//
+//  Created by Boris on 5/21/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
