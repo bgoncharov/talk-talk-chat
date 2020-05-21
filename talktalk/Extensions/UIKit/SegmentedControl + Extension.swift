b9e405575d7c2b6037d30ec03b43e8b4f8392c75
Boris Goncharov <bora4bora@gmail.com>
2020-05-21 11:00:20 -0700

    setup profile screen

---
+++ talktalk/Extensions/UIKit/SegmentedControl + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  SegmentedControl + Extension.swift
+//  talktalk
+//
+//  Created by Boris on 5/21/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+extension UISegmentedControl {
+    
+    convenience init(first: String, second: String){
+        self.init()
+        self.insertSegment(withTitle: first, at: 0, animated: true)
+        self.insertSegment(withTitle: second, at: 1, animated: true)
+        self.selectedSegmentIndex = 0
+    }
+}
--- talktalk/SetupProfileViewController.swift
+++ talktalk/SetupProfileViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class SetupProfileViewController: UIViewController {
     
-    let fillImageView = AddPhotoView()
+    let welcomeLabel = UILabel(text: "Set up your profile", font: .avenir26())
+    
+    let fullNameLabel = UILabel(text: "Full name")
+    let aboutMeLabel = UILabel(text: "About me")
+    let sexLabel = UILabel(text: "Sex")
+    
+    let fullNameTextField = OneLineTextField(font: .avenir20())
+    let aboutMeTextField = OneLineTextField(font: .avenir20())
+    let sexSegmentedControl = UISegmentedControl(first: "Male", second: "Female")
+    
+    let goToChatsButton = UIButton(title: "Go to chats!", backgroundColor: .buttonDark(), titleColor: .white, cornerRadius: 4)
+    
+    let fullImageView = AddPhotoView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +36,58 @@ class SetupProfileViewController: UIViewController {
 extension SetupProfileViewController {
     private func setupConstraints() {
         
-        fillImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(fillImageView)
+        let fullNameStackView = UIStackView(arrangedSubViews: [
+            fullNameLabel,
+            fullNameTextField],
+                                            axis: .vertical,
+                                            spacing: 0)
+        
+        let aboutMeStackView = UIStackView(arrangedSubViews: [
+            aboutMeLabel,
+            aboutMeTextField],
+                                           axis: .vertical,
+                                           spacing: 0)
+        
+        let sexStackView = UIStackView(arrangedSubViews: [
+            sexLabel,
+            sexSegmentedControl],
+                                       axis: .vertical,
+                                       spacing: 12)
+        
+        goToChatsButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        let stackView = UIStackView(arrangedSubViews: [
+            fullNameStackView,
+            aboutMeStackView,
+            sexStackView,
+            goToChatsButton],
+                                    axis: .vertical,
+                                    spacing: 40)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        fullImageView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        view.addSubview(fullImageView)
+        view.addSubview(stackView)
+        
+        fullImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fullImageView)
         
         NSLayoutConstraint.activate([
-            fillImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
-            fillImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            fullImageView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 40),
+            fullImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: fullImageView.bottomAnchor, constant: 40),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
     }
 }
