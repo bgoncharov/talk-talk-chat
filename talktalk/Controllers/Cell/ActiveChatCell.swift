5e9c4f8a90fd5e11e091ae43a90ccffc779ce80e
Boris Goncharov <bora4bora@gmail.com>
2020-05-22 18:47:58 -0700

    Active chat cell

---
+++ talktalk/Controllers/Cell/ActiveChatCell.swift
@@ -0,0 +1,13 @@
+//
+//  ActiveChatCell.swift
+//  talktalk
+//
+//  Created by Boris on 5/22/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class ActiveChatCell: UICollectionViewCell {
+    
+}
--- talktalk/Extensions/UIKit/UIFont + Extension.swift
+++ talktalk/Extensions/UIKit/UIFont + Extension.swift
@@ -17,4 +17,12 @@ extension UIFont {
     static func avenir26() -> UIFont? {
         return UIFont.init(name: "avenir", size: 26)
     }
+    
+    static func laoSangamMN20() -> UIFont? {
+        return UIFont.init(name: "Lao Sangan NM", size: 20)
+    }
+    
+    static func laoSangamMN18() -> UIFont? {
+        return UIFont.init(name: "Lao Sangam NM", size: 18)
+    }
 }
