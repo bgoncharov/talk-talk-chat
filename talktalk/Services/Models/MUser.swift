cb0b7a7469763ba4279330e66e592cb265f89ef8
Boris Goncharov <bora4bora@gmail.com>
2020-06-01 18:19:13 -0700

    users real time updates

--- talktalk/Services/Models/MUser.swift
+++ talktalk/Services/Models/MUser.swift
@@ -53,6 +53,23 @@ struct MUser: Hashable, Decodable {
         self.id = id
     }
     
+    init?(document: QueryDocumentSnapshot) {
+        let data = document.data()
+        guard let username = data["username"] as? String,
+        let email = data["email"] as? String,
+        let avatarStringURL = data["avatarStringURL"] as? String,
+        let description = data["description"] as? String,
+        let sex = data["sex"] as? String,
+        let id = data["uid"] as? String else { return nil }
+        
+        self.username = username
+        self.email = email
+        self.avatarStringURL = avatarStringURL
+        self.description = description
+        self.sex = sex
+        self.id = id
+    }
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
