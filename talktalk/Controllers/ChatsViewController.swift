3c55ba4210e93c36e84ecc60368dea2863881081
Boris Goncharov <bora4bora@gmail.com>
2020-06-04 11:08:42 -0700

    Sending text messages

--- talktalk/Controllers/ChatsViewController.swift
+++ talktalk/Controllers/ChatsViewController.swift
@@ -139,6 +139,15 @@ extension ChatsViewController: InputBarAccessoryViewDelegate {
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
         let message = MMessage(user: user, content: text)
         insertNewMessage(message: message)
+        FirestoreService.shared.sendMessage(chat: chat, message: message) { (result) in
+            switch result {
+                
+            case .success:
+                self.messagesCollectionView.scrollToBottom()
+            case .failure(let error):
+                self.showAlert(with: "Error", and: error.localizedDescription)
+            }
+        }
         inputBar.inputTextView.text = ""
     }
 }
